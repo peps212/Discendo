27dacd4139c9f8df63b9c6617f0cc06ab41eb587
peps212 <giuseppe.rapisarda12@gmail.com>
2023-04-20 15:52:26 +0100

    added webpack

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config, { webpack, isServer }) {
+
+    
+
+    config.experiments = {
+      asyncWebAssembly: true,
+      layers: true,
+    };
+
+    return config;
+  },
+  
 }
 
 module.exports = nextConfig
