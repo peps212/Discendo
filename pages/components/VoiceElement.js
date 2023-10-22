19306280bc81199c2de63246497b41152f3ec7df
peps212 <giuseppe.rapisarda12@gmail.com>
2023-10-22 19:29:56 +0100

    fix

--- pages/components/VoiceElement.js
+++ pages/components/VoiceElement.js
@@ -1,17 +1,14 @@
 import { useWhisper } from '@chengsokdara/use-whisper'
 import { useEffect } from 'react';
 
-export default function VoiceElement({disabled, onTranscript}) {
+export default function VoiceElement({onTranscript}) {
 
 
   const { transcript, startRecording } = useWhisper({
     apiKey: process.env.NEXT_PUBLIC_OPENAI_API_TOKEN, // YOUR_OPEN_AI_TOKEN
     nonStop: true, // keep recording as long as the user is speaking
-    stopTimeout: 2000,
-    whisperConfig: {
-        language: 'en',
-      }, // auto stop after 5 seconds
-    })
+    stopTimeout: 5000, // auto stop after 5 seconds
+  })
 
     useEffect(() => {
         if (transcript.text) {
@@ -23,7 +20,10 @@ export default function VoiceElement({disabled, onTranscript}) {
 
   return (
     <div>
-      <button disabled={disabled} className='bg-purple-500 rounded-lg px-4 py-2 text-white font-semibold focus:outline-none hover:bg-purple-600 transition-colors duration-300' onClick={() => startRecording()}>Talk</button>
+      <button className='bg-purple-500 rounded-lg px-4 py-2 text-white font-semibold focus:outline-none hover:bg-purple-600 transition-colors duration-300' onClick={() => startRecording()}>Talk</button>
     </div>
   )
+
+
+
 }
